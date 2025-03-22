773963813d4a91f77608186beef7725192d6f96f
Ray Martins <rhaymarthins@gmail.com>
2025-03-21 21:12:50 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask, request, jsonify
+import sqlite3
+
+app = Flask(__name__)
+
+# Inicialização do banco de dados (cria a tabela se não existir)
+def init_db():
+    with sqlite3.connect('database.db') as conn:
+        conn.execute("""CREATE TABLE IF NOT EXISTS livros(
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   titulo TEXT NOT NULL,
+                   categoria TEXT NOT NULL,
+                   autor TEXT NOT NULL,
+                   imagem_url TEXT NOT NULL
+                   )""")
+        print("Banco de dados criado!!")
+
+init_db()
+
+# Rota inicial com mensagem de boas vindas
+@app.route('/')
+def homepage():
+    return '<h2>Bem-vindos a API de livros feita em Flask!</h2>'
+
+# Rota para cadastrar novos livros
+@app.route('/doar', methods=['POST'])
+def doar():
+    dados = request.get_json()
+
+    titulo = dados.get('titulo')
+    categoria = dados.get('categoria')
+    autor = dados.get('autor')
+    imagem_url = dados.get('imagem_url')
+
+    if not all([titulo, categoria, autor, imagem_url]):
+        return jsonify({'erro': 'Todos os campos são obrigatórios'}), 400
+    
+    with sqlite3.connect('database.db') as conn:
+        conn.execute(f"""INSERT INTO livros (titulo, categoria, autor, imagem_url)
+                    VALUES (?,?,?,?)
+                    """, (titulo, categoria, autor, imagem_url))
+
+        conn.commit()
+
+        return jsonify({"mensagem": "Livro cadastrado com sucesso"}), 201
+
+# Rota para listar todos os livros
+@app.route('/livros', methods=['GET'])
+def listar_livros():
+    with sqlite3.connect('database.db') as conn:
+        livros = conn.execute("SELECT * FROM livros").fetchall()
+
+    livros_formatados = []
+
+    for livro in livros:
+        dicionario_livros = {
+            "id": livro[0],
+            "titulo": livro[1],
+            "categoria": livro[2],
+            "autor": livro[3],
+            "imagem_url": livro[4]
+        }
+        livros_formatados.append(dicionario_livros)
+    return jsonify(livros_formatados)
+
+# Inicia o servidor Flask
+if __name__ == "__main__":
+    app.run(debug=True)
