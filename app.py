c0fdc893caa6c298fee7b13e0ab41a2739f00f1e
Ray Martins <rhaymarthins@gmail.com>
2025-04-02 22:10:51 -0300

    att

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import sqlite3
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Inicialização do banco de dados (cria a tabela se não existir)
 def init_db():
